4a71c02ecaebaf94780bf240b299e65362ec36f6
Nelson <62866867+nelson26-shruti-mom@users.noreply.github.com>
2020-05-18 14:41:08 +0530

    Add files via upload

---
+++ Character_Pattern_2.py
@@ -0,0 +1,13 @@
+
+
+from string import ascii_lowercase
+for i in ascii_lowercase:
+    print(i,end=" ")
+
+print()
+
+
+print()
+
+for i in range (ord('A'), ord('Z')+1):
+    print(f"{chr(i)}",end= " ")
